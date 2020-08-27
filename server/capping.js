fc2e9864524a193d93be043a3289769806fe1f9f
Chris <guitar12698@hotmail.com>
2020-08-26 22:33:00 -0400

    started work on front end footer, header, main submission portal also installed nodemon

--- server/capping.js
+++ server/capping.js
@@ -7,6 +7,17 @@ app.get('/', function(request, response){
   response.sendFile('index.html', {root: './client/views'})
 });
 
+app.get('/index.html', function(req, res){
+  res.sendFile('index.html', {root: './client/views'})
+});
+
+app.get('/header', function(req, res){
+  res.sendFile('header.html', {root: './client/views'})
+});
+
+app.get('/footer', function(req, res){
+  res.sendFile('footer.html', {root: './client/views'})
+});
 
 app.listen(1337,() => console.log("running on port 1337: http://localhost:1337/"));
 //nodemon server/capping.js to run
