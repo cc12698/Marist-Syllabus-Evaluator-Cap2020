96b6513ad1c986b96962cabfcf4e2f293510df02
Chris <guitar12698@hotmail.com>
2020-08-26 21:46:15 -0400

    express npm

---
+++ server/capping.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('client/public'));
+
+app.get('/', function(request, response){
+  response.sendFile('index.html', {root: './client/views'})
+});
+
+
+app.listen(1337,() => console.log("running on port 1337: http://localhost:1337/"));
