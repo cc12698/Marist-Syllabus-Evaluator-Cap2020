8b6196c420dadd842605dc354f39fb06220f15b5
Chris <guitar12698@hotmail.com>
2020-08-28 16:23:34 -0400

    started to work on uploading the file

--- server/capping.js
+++ server/capping.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const upload = require('express-fileupload');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({type:'application/json'}));
 app.use(express.static('client/public'));
+app.use(upload());
 
 app.get('/', function(req, res){
   res.sendFile('index.html', {root: './client/views'})
@@ -19,6 +21,10 @@ app.get('/header', function(req, res){
 app.get('/footer', function(req, res){
   res.sendFile('footer.html', {root: './client/views'})
 });
+
+var docAdd = require('./controllers/doc');
+app.route('/api/sendDoc/doc')
+  .post(docAdd.saveDoc);
 
 app.listen(1337,() => console.log("running on port 1337: http://localhost:1337/"));
 //nodemon server/capping.js to run
