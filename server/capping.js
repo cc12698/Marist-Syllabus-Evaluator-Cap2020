e828304dfdc39611e8b29482432a20439fbae32c
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2020-08-29 19:08:30 -0400

    creating login page and updating index

--- server/capping.js
+++ server/capping.js
@@ -16,9 +16,17 @@ app.get('/header', function(req, res){
   res.sendFile('header.html', {root: './client/views'})
 });
 
+app.get('/headerLogin', function(req, res){
+  res.sendFile('headerLogin.html', {root: './client/views'})
+});
+
 app.get('/footer', function(req, res){
   res.sendFile('footer.html', {root: './client/views'})
 });
+
+app.get('/login', function(req, res){
+  res.sendFile('signIn.html', {root: './client/views'})
+})
 
 app.listen(1337,() => console.log("running on port 1337: http://localhost:1337/"));
 //nodemon server/capping.js to run
