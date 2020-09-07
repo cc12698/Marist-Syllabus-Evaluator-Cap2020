69af1190947438d1db5e3d025302d7e685486d6e
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2020-09-07 14:59:02 -0400

    fixing error with file upload

--- dataManager/index.js
+++ dataManager/index.js
@@ -1,8 +1,13 @@
 const multer = require("multer")
+var fs = require('fs');
 
 //store the uploaded file in uploads
 var storage =   multer.diskStorage({
   destination: function (req, file, callback) {
+    var dir = './uploads';
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
     callback(null, './uploads');
   },
   filename: function (req, file, callback) {
