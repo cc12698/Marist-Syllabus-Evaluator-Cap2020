187e8ff80212c3f489f3768dae05198147dd69bb
T0mmyBurda <Thomas.Burda1@marist.edu>
2020-09-10 20:51:02 -0400

    Have working loops and made log system, still bug with dictionary

--- routes/python/search.py
+++ routes/python/search.py
@@ -1,24 +1,25 @@
 import sys
 import re
 
-textFile = "./exampleText.txt"
+textFile = "exampleText.txt"
+logFile = "foundLog.txt"
 
 keywords = {    #list of regex commands ment to seach for items
-                "courseDes":      [] ,
+                "courseDes":      ["course description"] ,
                 "courseObj":      [] ,
                 "courseCred":     [] ,
                 "preReq":         [] ,
-                "gradeDet":       [] ,
+                "gradeDet":       ["grading:"] ,
                 "otherpolicies":  [] ,
                 "instrName":      [] ,
-                "instrContact":   [] ,
+                "instrContact":   [" (.)*" , "contact:" , "e(-| )mail:"] ,
                 "democonsistant": [] , # not sure if this can be checked with keywords
                 "assesMethod":    [] ,
                 "rubrics":        [] , # may be missing
                 "biblio":         [] ,
-                "assignments":    [] ,
+                "assignments":    ["assignments" , "bee"] ,
                 "taskCrit":       [] ,
-                "courseNum":      ["[0-9]{3}( )*(N|L|n|l)( )*[0-9]{3}" , "course()*number"] ,
+                "courseNum":      ["[0-9]{3}( )*(N|L|n|l)( )*[0-9]{3}" , "course( )*number" , "class( )*number" , "[0-9]{3}( )*(N|L|n|l)"] ,
                 "format":         [] ,
                 "attenPol":       [] ,
                 "reqRead":        [] ,
@@ -30,7 +31,7 @@ keywords = {    #list of regex commands ment to seach for items
                 "diversity":      [] , # not technically req
             }
 
-found = {   #list of regex commands ment to seach for items
+found = {   #empty dictionary of arrays to store any matches to analyize later
             "courseDes":      [] ,
             "courseObj":      [] ,
             "courseCred":     [] ,
@@ -57,13 +58,38 @@ found = {   #list of regex commands ment to seach for items
             "diversity":      [] , # not technically req
         }
 
-def checkFile()
-    found = len(cars)
+def checkFileAnal():
+    print("checking file...")
+    print(keywords["courseDes"][1])
+
+    o = open(logFile, "a")
+    o.write("\n\nOutput for " + "textFile") #text file will be the sylibus being evaluated
+
+    s = open(textFile , "r")
+
+    for key in keywords: #loops through entire dictionary
+        o.write("\nResults for " + key)
+        for line in s: #loops through each line of sylibus
+            print(keywords[key])
+            for i in keywords[key]: #loops through each regex search
+                print("loop")
+                result = re.search(keywords[key][i] , line) #searches line for regex command
+                if(result != "none"):
+                    print(result)
+                    found[key] = result
+                    o.write("Match to " + keywords[key][i] + " in line " + line + ": " + result + "\n")
+
+    return found
+
+def checkFileFast():
+    found = len()
 
     f = open(textFile , "r")
     for i in keywords:
         for line in f:
-            if keywords[i] in line
+            if keywords[i] in line:
                 found[i] = line
 
     return found
+
+checkFileAnal()
