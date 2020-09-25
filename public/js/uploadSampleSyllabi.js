8391821daf9c63bd6429e55cc75bbe8f1ffeaaca
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2020-09-24 20:40:23 -0400

    restricting file upload type

--- public/js/uploadSampleSyllabi.js
+++ public/js/uploadSampleSyllabi.js
@@ -1,14 +1,7 @@
 function getFileInfo(){
-	var file = document.getElementById('sample_syl').files[0];
+	var file = document.getElementById('upload').files[0];
 
 	// console.log(file.name);
 	$('#fileNameSyl').val(file.name);
-  var fileName = file.name;
-  var ext = fileName.split('.').pop().toLowerCase();
-  if($.inArray(ext, ['doc','docx','pages','pdf']) == -1) {
-      alert('Invalid File Type.\nPlease Upload a Word Document, PDF, or Pages File.');
-      $('#fileNameSyl').value = "";
-      $('#sample_syl').value = "";
-  }
-  // console.log($('#fileNameSyl').val());
+
 }
