95e6221ab938c5678b45146e355bb0561e96ac57
Chris <guitar12698@hotmail.com>
2020-10-18 11:24:06 -0400

    checks to see which items have been checked

--- public/js/sampleSyllabi.js
+++ public/js/sampleSyllabi.js
@@ -1,1 +1,7 @@
-
+function getCheckedCheckboxesFor(checkboxName) {
+    var checkboxes = document.querySelectorAll('input[name="' + checkboxName + '"]:checked'), values = [];
+    Array.prototype.forEach.call(checkboxes, function(el) {
+        values.push(el.value);
+    });
+    return values;
+}
