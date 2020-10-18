8d4b4599060776fef5e2b7f344cf1f4ce93ad7ac
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2020-10-18 12:38:06 -0400

    fixing html error and creating unique name for each item

--- public/js/sampleSyllabi.js
+++ public/js/sampleSyllabi.js
@@ -1,7 +1,1 @@
-function getCheckedCheckboxesFor(checkboxName) {
-    var checkboxes = document.querySelectorAll('input[name="' + checkboxName + '"]:checked'), values = [];
-    Array.prototype.forEach.call(checkboxes, function(el) {
-        values.push(el.value);
-    });
-    return values;
-}
+
